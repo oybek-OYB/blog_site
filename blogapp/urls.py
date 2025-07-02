3f4bc584d1a53c7d9c096fc72a95c5f3b6c9ba6c
Oybek <oybektorayev774@gmail.com>
2025-07-02 10:07:03 +0500

    add forms , update detail.html, models, urls, views

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'blogapp'
+app_name = 'blog'
 urlpatterns = [
 
     path('', views.PostListView.as_view(), name='post_list'),
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import get_object_or_404, render
 
+from .forms import CommentForm
 from .models import Post
 from django.views.generic import ListView
+
 
 
 class PostListView(ListView):
@@ -13,4 +15,23 @@ class PostListView(ListView):
 
 def post_detail(request, year, month, day, slug):
     post = get_object_or_404(Post, slug=slug, status="published", publish__year=year, publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form,
+                   })
